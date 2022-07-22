b3ae30f87b2a94c6cf753807d33d353625cecae5
Rafael <rafapgbr@gmail.com>
2022-07-22 10:36:47 -0300

    feat: inserindo o "await" no "server.init", pois, agora que temos o banco de dados, a inicialização dos métodos deverá ser assíncrona.

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -9,9 +9,13 @@
 import { SetupServer } from '@src/server';
 import supertest from 'supertest';
 
-beforeAll(() => {
+let server: SetupServer;
+
+beforeAll(async() => {
   //O "beforeAll" será executado antes de todos os testes da aplicação. Ele inicializará a aplicação no modo de testes.
-  const server = new SetupServer();
-  server.init();
+  server = new SetupServer();
+  await server.init();
   global.testRequest = supertest(server.getApp());
 });
+
+afterAll(async() => await server.close()); //Após os testes passarem, o banco de dados será encerrado.
