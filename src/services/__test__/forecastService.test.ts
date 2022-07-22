f161d62824f9cd89b4bbb02a666222cd5af8c97c
Rafael <rafapgbr@gmail.com>
2022-07-22 10:14:24 -0300

    test: inserção de teste para verificar se os erros estão sendo lançados corretamente.

--- src/services/__test__/forecastService.test.ts
+++ src/services/__test__/forecastService.test.ts
@@ -1,13 +1,20 @@
-import { StormGlass } from '@src/clients/stormGlass';
+import {StormGlass} from '@src/clients/stormGlass';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 import {Beach, BeachPosition, Forecast} from "@src/services/forecastService";
 
 jest.mock('@src/clients/stormGlass'); //Estaremos mockando o cliente, ou seja, mockaremos o resultado do retorno dos métodos do cliente, que são os dados normalizados.
 
+const mockedStormGlassService = new StormGlass as jest.Mocked<StormGlass>; //Estamos adicionando os tipos do "Jest" ao serviço "StormGlass".
+
+
 describe('Forecast Service', () =>{
 
     it('deve retornar a previsão do tempo para uma lista de praias.', async () =>{
-        StormGlass.prototype.fetchPoints = jest.fn().mockResolvedValue(stormGlassNormalizedResponseFixture);
+        // StormGlass.prototype.fetchPoints = jest.fn().mockResolvedValue(stormGlassNormalizedResponseFixture);
+
+        mockedStormGlassService.fetchPoints.mockResolvedValue(
+            stormGlassNormalizedResponseFixture
+        );
 
         const beaches: Beach[] = [ //Esse será um exemplo de praias da aplicação. Nesse exemplo, o usuário terá apenas uma praia cadastrada.
             {
@@ -82,9 +89,35 @@ describe('Forecast Service', () =>{
             },
         ];
 
-        const forecast = new Forecast(new StormGlass()); //O "Forecast" vai receber uma instância do "StormGlass", que é o cliente de API.
+        const forecast = new Forecast(mockedStormGlassService); //O "Forecast" vai receber uma instância do mock do "StormGlass", que é o cliente de API.
         const beachesWithRating = await forecast.processForecastForBeaches(beaches); //Esse serviço também receberá uma lista de praias com o forecast correto.
 
         expect(beachesWithRating).toEqual(expectedResponse); //A resposta do método de realizar o processamento para as praias deverá ser igual ao JSON que possui os dados da praia, o rating e outras informações sobre a previsão do tempo naquele instante.
-    })
+    });
+
+    it('deve retornar uma lista vazia se o usuário não possui nenhuma praia cadastrada', async() => {
+        const forecast = new Forecast();
+        const response = await forecast.processForecastForBeaches([]);
+
+        expect(response).toEqual([]); //Se o usuário não possui nenhuma praia cadastrada, deve ser retornado um array vazio.
+    });
+
+    it('deve lançar um internal processing error quando algo de errado ocorrer durante o processo de rating', async() => {
+        const beaches: Beach[] = [ //Estamos simulando que o usuário, nesse teste, terá apenas uma praia cadastrada.
+            {
+                lat: -33.792726,
+                lng: 151.289824,
+                name: 'Manly',
+                position: BeachPosition.E,
+                user: 'some-id'
+            },
+        ];
+
+        mockedStormGlassService.fetchPoints.mockRejectedValue(
+            'Error fetching data'
+        );
+
+        const forecast = new Forecast(mockedStormGlassService);
+        await expect(forecast.processForecastForBeaches(beaches)).rejects.toThrow(Error); //Queremos que o método do service, que processará o tempo para as praias do usuário, retorne um erro.
+    });
 })
