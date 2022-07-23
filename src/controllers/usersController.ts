b3808076c00d13f895bb414eab594632039c73c1
Rafael <rafapgbr@gmail.com>
2022-07-23 18:33:40 -0300

    feat: adição de validação para enviar um erro semântico.

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,14 +1,22 @@
 import {Request, Response} from "express";
 import {Controller, Post} from "@overnightjs/core";
 import {User} from "@src/models/user";
+import {BaseController} from "@src/controllers/indexController";
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
 
     @Post('')
     public async create(req: Request, res: Response): Promise<void> {
-        const user = new User(req.body);
-        const newUser = await user.save();
-        res.status(201).send(newUser);
+
+        try {
+            const user = new User(req.body);
+            const newUser = await user.save();
+            res.status(201).send(newUser);
+        }catch(error){
+            if(error instanceof Error){
+                this.sendCreateUpdateErrorResponse(res, error);
+            }
+        }
     }
 }
