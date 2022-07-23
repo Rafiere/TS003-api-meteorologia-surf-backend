a1fd28a5cac0984a3b5749d137dec422f50166f9
Rafael <rafapgbr@gmail.com>
2022-07-23 12:50:40 -0300

    test: inserção de testes para verificar se o "500" será retornado corretamente, inserção de teste para resetar e salvar um elemento no banco de dados antes de cada teste e inserção de teste para verificar se o controller retornará uma resposta como esperada.

--- test/functional/ForecastController.test.ts
+++ test/functional/ForecastController.test.ts
@@ -6,52 +6,68 @@
  * que ele é um teste end-to-end.
  */
 
-describe('Beach forecast functional tests', () => {
-  //Estamos descrevendo um bloco de testes. Dentro desse bloco teremos vários outros testes.
-  it('should return a forecast with just a few times', async () => {
-    //O "it" contém a descrição do teste.
-    const { body, status } = await global.testRequest.get('/forecast');
-    expect(status).toBe(200);
+import {Beach, BeachPosition} from "@src/models/beach";
+import nock from 'nock';
+import api_forecast_response_1_beach from "@test/fixtures/api_forecast_response_1_beach.json"
+import stormglass_weather_3_hours from "@test/fixtures/stormglass_weather_3_hours.json"
 
-    expect(body).toEqual([
-      {
-        time: '2020-04-26T00:00:00+00:00',
-        forecast: [
-          {
+describe('Beach forecast functional tests', () => {
+
+    beforeEach(async () => {
+        await Beach.deleteMany({});
+
+        const defaultBeach = {
             lat: -33.792726,
             lng: 151.289824,
             name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-          },
-        ],
-      },
-    ]);
-  });
+            position: BeachPosition.E
+        };
+
+        const beach = new Beach(defaultBeach);
+        await beach.save();
+    });
+
+    //Estamos descrevendo um bloco de testes. Dentro desse bloco teremos vários outros testes.
+    it('should return a forecast with just a few times', async () => {
+        //O "it" contém a descrição do teste.
+
+        nock('https://api.stormglass.io:443', {
+            encodedQueryParams: true,
+            reqheaders: {
+                Authorization: (): boolean => true,
+            },
+        })
+            .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+            .get('/v2/weather/point')
+            .query({
+                lat: '-33.792726',
+                lng: '151.289824',
+                params: /(.*)/,
+                source: 'noaa',
+            })
+            .reply(200, stormglass_weather_3_hours);
+
+        const { body, status } = await global.testRequest.get('/forecast');
+
+        expect(status).toBe(200);
+
+        expect(body).toEqual(api_forecast_response_1_beach);
+    });
+
+    it('should return 500 if something goes wrong during the processing', async () => {
+        nock('https://api.stormglass.io:443', {
+            encodedQueryParams: true,
+            reqheaders: {
+                Authorization: (): boolean => true,
+            },
+        })
+            .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+            .get('/v2/weather/point')
+            .query({ lat: '-33.792726', lng: '151.289824' })
+            .replyWithError('Something went wrong');
+
+        const { status } = await global.testRequest.get(`/forecast`);
+
+        expect(status).toBe(500);
+    });
 });
