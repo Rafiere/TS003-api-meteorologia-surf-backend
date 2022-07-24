f1989beae627949d440a81d6616356d2d0bfc789
Rafael <rafapgbr@gmail.com>
2022-07-23 23:16:31 -0300

    feat: inserção de endpoint para buscar e autenticar um determinado usuário, através da geração de um token JSON.

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -2,6 +2,7 @@ import {Request, Response} from "express";
 import {Controller, Post} from "@overnightjs/core";
 import {User} from "@src/models/user";
 import {BaseController} from "@src/controllers/indexController";
+import AuthService from "@src/services/authService";
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -18,5 +19,28 @@ export class UsersController extends BaseController {
                 this.sendCreateUpdateErrorResponse(res, error);
             }
         }
+    }
+
+    @Post('authenticate')
+    public async authenticate(req: Request, res: Response): Promise<Response> {
+
+        const user = await User.findOne({email: req.body.email}); //Estamos buscando o usuário pelo email.
+        if(!user){ //Se nenhum usuário for retornado do banco de dados, esse bloco de código será executado.
+            return res.status(401).send({
+                code: 401,
+                error: 'User not found!'
+            });
+        }
+
+        if(!(await AuthService.comparePasswords(req.body.password, user.password))){
+            return res.status(401).send({
+                code: 401,
+                error: 'Password does not match!'
+            });
+        }
+
+        const token = AuthService.generateToken(user.toJSON()); //Estamos transformando o usuário obtido do banco de dados em um JSON para gerarmos o token.
+
+        return res.status(200).send({... user.toJSON(), ...{token}});
     }
 }
