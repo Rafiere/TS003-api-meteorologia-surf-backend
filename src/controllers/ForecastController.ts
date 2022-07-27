5a8f5f4a894b063f5185093e4e33a0726175de08
Rafael <rafapgbr@gmail.com>
2022-07-26 22:09:06 -0300

    feat: adição de mensagens de erro únicas nos controllers.

--- src/controllers/ForecastController.ts
+++ src/controllers/ForecastController.ts
@@ -4,6 +4,7 @@ import {Forecast} from "@src/services/forecastService";
 import {Beach} from "@src/models/beach";
 import {authMiddleware} from "@src/middlewares/auth";
 import logger from "@src/logger";
+import {BaseController} from "@src/controllers/indexController";
 
 /**
  * Os controllers são utilizados para receber uma determinada requisição e
@@ -17,7 +18,7 @@ const forecast = new Forecast();
 
 @Controller('forecast')
 @ClassMiddleware(authMiddleware) //Esse middleware garantirá que o usuário que fez a requisição esteja inserido dentro da requisição.
-export class ForecastController {
+export class ForecastController extends BaseController {
   @Get('')
   public async getForecastForgeLoggedUser(req: Request, res: Response): Promise<void> {
 
@@ -29,8 +30,7 @@ export class ForecastController {
       res.status(200).send(forecastData);
 
     }catch(error: unknown){
-      logger.error(error);
-      res.status(500).send({error: 'Something went wrong!'});
+      this.sendErrorResponse(res, {code: 500, message: 'Something went wrong!'});
     }
   }
 }
