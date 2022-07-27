fe666bc5230ecdec638d6b07e563ec612bc3363d
Rafael <rafapgbr@gmail.com>
2022-07-26 23:04:50 -0300

    feat: inserção do endpoint "/me" para obter as informações do usuário logado.

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,9 +1,10 @@
 import {Request, Response} from "express";
-import {Controller, Post} from "@overnightjs/core";
+import {Controller, Get, Middleware, Post} from "@overnightjs/core";
 import {User} from "@src/models/user";
 import {BaseController} from "@src/controllers/indexController";
 import AuthService from "@src/services/authService";
 import logger from "@src/logger";
+import {authMiddleware} from "@src/middlewares/auth";
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -44,5 +45,25 @@ export class UsersController extends BaseController {
         const token = AuthService.generateToken(user.toJSON()); //Estamos transformando o usuário obtido do banco de dados em um JSON para gerarmos o token.
 
         return res.status(200).send({... user.toJSON(), ...{token}});
+    }
+
+    /**
+     * Esse endpoint retornará as informações do usuário que tiver o token. Ele será
+     * utilizado pelo front-end para obter as informações do usuário.
+     */
+
+    @Get('me')
+    @Middleware(authMiddleware)
+    public async me(req: Request, res: Response): Promise<Response> {
+        const email = req.decoded ? req.decoded.email : undefined; //Se o email existir, retornaremos o email, caso contrário, retornaremos "undefined'.
+        const user = await User.findOne({email});
+
+        if(!user){
+            return this.sendErrorResponse(res, {
+                code: 404,
+                message: 'User not found!',
+            });
+        }
+        return res.send({ user });
     }
 }
