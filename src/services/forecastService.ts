217ae6f162d15dc53eb39e7ce420453ef2fd844b
Rafael <rafapgbr@gmail.com>
2022-07-27 14:45:28 -0300

    refactor! retirada de log não utilizado.

--- src/services/forecastService.ts
+++ src/services/forecastService.ts
@@ -65,7 +65,6 @@ export class Forecast {
                 forecast: _.orderBy(t.forecast, ['rating'], ['desc']) //Vamos pegar todos os forecasts de um horário e vamos ordenar os horários por rating na ordem decrescente.
             }))
         }catch(error: unknown){
-            logger.error(error);
             throw new ForecastProcessingInternalError((error as Error).message);
         }
     }
